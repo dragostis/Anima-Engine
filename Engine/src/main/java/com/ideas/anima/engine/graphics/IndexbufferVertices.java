187708cdd00011f71765c8c43d17f1b065147534
dragostis <dragostiselice@gmail.com>
2014-04-18 23:40:13 +0300

    Renamed the vertex handlers to better explain their use.

--- Engine/src/main/java/com/ideas/anima/engine/graphics/IndexbufferVertices.java
+++ Engine/src/main/java/com/ideas/anima/engine/graphics/IndexbufferVertices.java
@@ -2,19 +2,22 @@ package com.ideas.anima.engine.graphics;
 
 import android.opengl.GLES30;
 
-import com.ideas.anima.engine.data.Vertices;
+import com.ideas.anima.engine.data.blocks.VerticesBlock;
 
-public class IndexBufferArray implements ModelArray {
-    private static final int bytesPerFloat = 4;
-    private static final int positionDataSize = 3;
-    private static final int textCoordDataSize = 2;
-    private static final int stride = (positionDataSize + textCoordDataSize)
-            * bytesPerFloat;
-    private Vertices vertices;
+public class IndexbufferVertices implements Vertices {
+    private static final int BYTES_PER_FLOAT = 4;
+    private static final int POSITION_DATA_SIZE = 3;
+    private static final int TEXT_COORD_DATA_SIZE = 2;
+    private static final int STRIDE = (POSITION_DATA_SIZE + TEXT_COORD_DATA_SIZE)
+            * BYTES_PER_FLOAT;
+    private VerticesBlock verticesBlock;
     private int bufferHandle;
+    private int numOfVertices;
 
-    public IndexBufferArray(Vertices vertices) {
-        this.vertices = vertices;
+    public IndexbufferVertices(VerticesBlock verticesBlock) {
+        this.verticesBlock = verticesBlock;
+
+        allocateArray(null);
     }
 
     @Override
@@ -25,16 +28,18 @@ public class IndexBufferArray implements ModelArray {
         GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, buffers[0]);
         GLES30.glBufferData(
                 GLES30.GL_ARRAY_BUFFER,
-                vertices.getFloatBuffer().capacity() * bytesPerFloat,
-                vertices.getFloatBuffer(),
+                verticesBlock.getFloatBuffer().capacity() * BYTES_PER_FLOAT,
+                verticesBlock.getFloatBuffer(),
                 GLES30.GL_STATIC_DRAW
         );
         GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, 0);
 
         bufferHandle = buffers[0];
 
-        vertices.getFloatBuffer().limit(0);
-        vertices.setFloatBuffer(null);
+        numOfVertices = verticesBlock.getFloatBuffer().capacity() / 5;
+
+        verticesBlock.getFloatBuffer().limit(0);
+        verticesBlock.setFloatBuffer(null);
     }
 
     @Override
@@ -43,24 +48,24 @@ public class IndexBufferArray implements ModelArray {
         GLES30.glEnableVertexAttribArray(scene.getPositionHandle());
         GLES30.glVertexAttribPointer(
                 scene.getPositionHandle(),
-                positionDataSize,
+                POSITION_DATA_SIZE,
                 GLES30.GL_FLOAT,
                 false,
-                stride,
+                STRIDE,
                 0
         );
         GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, bufferHandle);
         GLES30.glEnableVertexAttribArray(scene.getTextCoordHandle());
         GLES30.glVertexAttribPointer(
                 scene.getTextCoordHandle(),
-                textCoordDataSize,
+                TEXT_COORD_DATA_SIZE,
                 GLES30.GL_FLOAT,
                 false,
-                stride,
-                positionDataSize * bytesPerFloat
+                STRIDE,
+                POSITION_DATA_SIZE * BYTES_PER_FLOAT
         );
 
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, vertices.getFloatBuffer().capacity() / 5);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, numOfVertices);
 
         GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, 0);
     }
--- Engine/src/main/java/com/ideas/anima/engine/graphics/Vertices.java
+++ Engine/src/main/java/com/ideas/anima/engine/graphics/Vertices.java
@@ -1,6 +1,6 @@
 package com.ideas.anima.engine.graphics;
 
-public interface ModelArray {
+public interface Vertices {
     public void allocateArray(float[] array);
 
     public void draw(Scene scene);
