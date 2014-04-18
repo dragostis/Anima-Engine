9b83f43a5e85ac663ada5cce5024f2c35fe444ab
dragostis <dragostiselice@gmail.com>
2014-04-18 23:41:22 +0300

    Moved RenderObject to the objects package.

--- Engine/src/main/java/com/ideas/anima/engine/graphics/objects/RenderedObject.java
+++ Engine/src/main/java/com/ideas/anima/engine/graphics/objects/RenderedObject.java
@@ -1,19 +1,22 @@
-package com.ideas.anima.engine.graphics;
+package com.ideas.anima.engine.graphics.objects;
 
+import android.opengl.GLES30;
 import android.opengl.Matrix;
 
 import com.ideas.anima.engine.gameplay.GameObject;
+import com.ideas.anima.engine.graphics.Scene;
+import com.ideas.anima.engine.graphics.Vector;
 
 public abstract class RenderedObject extends GameObject {
     private float[] modelMatrix = new float[16];
 
-    public RenderedObject() {
+    protected RenderedObject() {
         super();
 
         updateModelMatrix();
     }
 
-    public RenderedObject(Vector position, Vector rotation, Vector scale) {
+    protected RenderedObject(Vector position, Vector rotation, Vector scale) {
         super(position, rotation, scale);
 
         updateModelMatrix();
@@ -31,5 +34,16 @@ public abstract class RenderedObject extends GameObject {
         Matrix.scaleM(modelMatrix, 0, getScale().x, getScale().y, getScale().z);
     }
 
-    public abstract void draw(float deltaTime, Scene scene);
+    public void draw(Scene scene) {
+        updateModelMatrix();
+
+        Matrix.multiplyMM(modelMatrix, 0, scene.getWorld().getViewMatrix(), 0, modelMatrix, 0);
+        Matrix.multiplyMM(modelMatrix, 0, scene.getWorld().getProjectionMatrix(), 0, modelMatrix, 0);
+
+        GLES30.glUniformMatrix4fv(scene.getMvpMatrixHandle(), 1, false, modelMatrix, 0);
+
+        drawObject(scene);
+    }
+
+    protected abstract void drawObject(Scene scene);
 }
