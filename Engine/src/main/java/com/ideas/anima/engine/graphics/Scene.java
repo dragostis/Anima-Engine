fea0da2bc2b84b02a13268816d5c5a13b4b715c0
dragostis <dragostiselice@gmail.com>
2014-04-18 23:42:39 +0300

    Changed Scene to handle all the program handles.

--- Engine/src/main/java/com/ideas/anima/engine/graphics/Scene.java
+++ Engine/src/main/java/com/ideas/anima/engine/graphics/Scene.java
@@ -2,13 +2,23 @@ package com.ideas.anima.engine.graphics;
 
 import android.opengl.GLES30;
 
-public class Scene {
+public abstract class Scene {
     private Program program;
+    private World world;
     private int positionHandle;
     private int textCoordHandle;
+    private int mvpMatrixHandle;
+    private int textureLocationHandle;
 
-    public Scene(Program program) {
+    public Scene(Program program, World world) {
         this.program = program;
+        this.world = world;
+
+        getHandles();
+    }
+
+    public World getWorld() {
+        return world;
     }
 
     public int getPositionHandle() {
@@ -19,14 +29,28 @@ public class Scene {
         return textCoordHandle;
     }
 
+    public int getMvpMatrixHandle() {
+        return mvpMatrixHandle;
+    }
+
+    public int getTextureLocationHandle() {
+        return textureLocationHandle;
+    }
+
     public void use() {
         program.use();
-
-        updateHandles();
     }
 
-    private void updateHandles() {
+    private void getHandles() {
         positionHandle = GLES30.glGetAttribLocation(program.getProgramHandle(), "a_Position");
         textCoordHandle = GLES30.glGetAttribLocation(program.getProgramHandle(), "a_TextCoord");
+        mvpMatrixHandle = GLES30.glGetUniformLocation(program.getProgramHandle(), "u_MVPMatrix");
+        textureLocationHandle = GLES30.glGetUniformLocation(program.getProgramHandle(), "u_Texture");
+
+        getUniformHandles();
     }
+
+    public abstract void getUniformHandles();
+
+    public abstract void draw();
 }
