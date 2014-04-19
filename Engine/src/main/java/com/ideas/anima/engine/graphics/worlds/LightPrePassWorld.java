6ca32fcbf702b3eaf0983fea180c8c2f69f90e60
dragostis <dragostiselice@gmail.com>
2014-04-19 15:23:58 +0300

    Added a light pre pass world stub.

---
+++ Engine/src/main/java/com/ideas/anima/engine/graphics/worlds/LightPrePassWorld.java
@@ -0,0 +1,16 @@
+package com.ideas.anima.engine.graphics.worlds;
+
+import com.ideas.anima.engine.Game;
+import com.ideas.anima.engine.graphics.World;
+import com.ideas.anima.engine.graphics.objects.Camera;
+
+public class LightPrePassWorld extends World {
+    public LightPrePassWorld(Game game, Camera camera) {
+        super(game, camera);
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
