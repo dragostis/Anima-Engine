117f98677ffbc7ee5bfd584950d8696c60bb42a0
dragostis <dragostiselice@gmail.com>
2014-04-23 11:50:46 +0300

    Added basic material properties to the model class.

--- Engine/src/main/java/com/ideas/anima/engine/graphics/objects/Model.java
+++ Engine/src/main/java/com/ideas/anima/engine/graphics/objects/Model.java
@@ -1,5 +1,7 @@
 package com.ideas.anima.engine.graphics.objects;
 
+import android.opengl.GLES30;
+
 import com.ideas.anima.engine.data.blocks.ModelBlock;
 import com.ideas.anima.engine.graphics.IndexbufferVertices;
 import com.ideas.anima.engine.graphics.Scene;
@@ -11,6 +13,9 @@ public class Model extends RenderedObject {
     private Vertices vertices;
     private Texture texture;
     private Texture normalMap;
+    private Vector ambientColor = new Vector(0.05f);
+    private Vector diffuseColor = new Vector(0.7f);
+    private Vector specularColor = new Vector(0.0f);
 
     public Model(ModelBlock modelBlock) {
         vertices = new IndexbufferVertices(modelBlock.getVerticesBlock());
@@ -26,10 +31,64 @@ public class Model extends RenderedObject {
         normalMap = new Texture(modelBlock.getNormalMapBlock());
     }
 
+    public Vector getAmbientColor() {
+        return ambientColor;
+    }
+
+    public void setAmbientColor(Vector ambientColor) {
+        this.ambientColor = ambientColor;
+    }
+
+    public Vector getDiffuseColor() {
+        return diffuseColor;
+    }
+
+    public void setDiffuseColor(Vector diffuseColor) {
+        this.diffuseColor = diffuseColor;
+    }
+
+    public Vector getSpecularColor() {
+        return specularColor;
+    }
+
+    public void setSpecularColor(Vector specularColor) {
+        this.specularColor = specularColor;
+    }
+
     @Override
     protected void drawObject(Scene scene) {
         texture.bind(0, scene.getTextureLocationHandle());
-        normalMap.bind(1, scene.getNormalMapLocationHandle());
+
+        if (scene.getNormalMapLocationHandle() != -1) {
+            normalMap.bind(1, scene.getNormalMapLocationHandle());
+        }
+
+        if (scene.getAmbientHandle() != -1) {
+            GLES30.glUniform3f(
+                    scene.getAmbientHandle(),
+                    ambientColor.x,
+                    ambientColor.y,
+                    ambientColor.z
+            );
+        }
+
+        if (scene.getDiffuseHandle() != -1) {
+            GLES30.glUniform3f(
+                    scene.getDiffuseHandle(),
+                    diffuseColor.x,
+                    diffuseColor.y,
+                    diffuseColor.z
+            );
+        }
+
+        if (scene.getSpecularHandle() != -1) {
+            GLES30.glUniform3f(
+                    scene.getSpecularHandle(),
+                    specularColor.x,
+                    specularColor.y,
+                    specularColor.z
+            );
+        }
 
         vertices.draw(scene);
     }
