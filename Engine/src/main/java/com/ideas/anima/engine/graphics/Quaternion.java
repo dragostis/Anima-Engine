0bba58cf6be9ee4c4b31b20f775b3a51855e9beb
bteleaga <teleaga.bogdan@yahoo.com>
2014-05-20 17:58:21 +0300

    Added getters and setters for Quaternion.

--- Engine/src/main/java/com/ideas/anima/engine/graphics/Quaternion.java
+++ Engine/src/main/java/com/ideas/anima/engine/graphics/Quaternion.java
@@ -21,6 +21,38 @@ public class Quaternion {
         w =  (float) Math.cos(Math.toRadians(angle / 2.0f));
     }
 
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public float getW() {
+        return w;
+    }
+
+    public void setW(float w) {
+        this.w = w;
+    }
+
     public void multiply(Quaternion quaternion) {
         x = quaternion.w * x + quaternion.x * w + quaternion.y * z - quaternion.z * y;
         y = quaternion.w * y + quaternion.x * z + quaternion.y * w - quaternion.z * x;
