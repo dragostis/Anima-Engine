cc11bf4f22d7a0c99b0631f352876562900ac955
dragostis <dragostiselice@gmail.com>
2014-05-25 14:32:14 +0300

    Added a base Interpolator class.

--- Engine/src/main/java/com/ideas/anima/engine/gameplay/interpolators/Interpolator.java
+++ Engine/src/main/java/com/ideas/anima/engine/gameplay/interpolators/Interpolator.java
@@ -1,5 +1,20 @@
 package com.ideas.anima.engine.gameplay.interpolators;
 
-public interface Interpolator {
-    public void getInterpolation(float t);
+public abstract class Interpolator {
+    private Interpolatable[] interpolatables;
+    private float totalTime;
+    private float currentTime;
+
+    public Interpolator(float totalTime, Interpolatable... interpolatables) {
+        this.interpolatables = interpolatables;
+        this.totalTime = totalTime;
+    }
+
+    public void update(float deltaTime) {
+        currentTime += deltaTime;
+
+        if (currentTime <= totalTime) interpolate(currentTime / totalTime);
+    }
+
+    protected abstract void interpolate(float ratio);
 }
