091a717f719edba17b473df14062d42fd85049ba
dragostis <dragostiselice@gmail.com>
2014-05-25 15:58:33 +0300

    Reorganised mappings.

--- Engine/src/main/java/com/ideas/anima/engine/gameplay/interpolators/mappings/ReverseMapping.java
+++ Engine/src/main/java/com/ideas/anima/engine/gameplay/interpolators/mappings/ReverseMapping.java
@@ -1,4 +1,6 @@
-package com.ideas.anima.engine.gameplay.interpolators;
+package com.ideas.anima.engine.gameplay.interpolators.mappings;
+
+import com.ideas.anima.engine.gameplay.interpolators.Mapping;
 
 public class ReverseMapping implements Mapping {
     @Override
