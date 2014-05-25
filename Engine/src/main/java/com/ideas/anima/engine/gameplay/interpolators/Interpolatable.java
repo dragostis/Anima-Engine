6388263423a6a75f6851fe6eb2b49299e76136e4
dragostis <dragostiselice@gmail.com>
2014-05-25 16:40:34 +0300

    Changed Interpolatable to make it similar to linear combinations.

--- Engine/src/main/java/com/ideas/anima/engine/gameplay/interpolators/Interpolatable.java
+++ Engine/src/main/java/com/ideas/anima/engine/gameplay/interpolators/Interpolatable.java
@@ -1,7 +1,5 @@
 package com.ideas.anima.engine.gameplay.interpolators;
 
 public interface Interpolatable<T> {
-    public void multiply(float scalar);
-
-    public void add(T object);
+    public void combine(float ratio1, Interpolatable<T> object2, float ratio2);
 }
