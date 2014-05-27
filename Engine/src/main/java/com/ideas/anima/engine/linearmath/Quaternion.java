7593c4d4f1bf393e2e39ef75d0ce321aaf39e739
dragostis <dragostiselice@gmail.com>
2014-05-27 17:38:05 +0300

    Removed the matrix dependent code from the Quaternion class.

--- Engine/src/main/java/com/ideas/anima/engine/linearmath/Quaternion.java
+++ Engine/src/main/java/com/ideas/anima/engine/linearmath/Quaternion.java
@@ -1,4 +1,4 @@
-package com.ideas.anima.engine.graphics;
+package com.ideas.anima.engine.linearmath;
 
 public class Quaternion {
     private float x;
@@ -15,9 +15,9 @@ public class Quaternion {
 
         float sin = (float) Math.sin(Math.toRadians(angle / 2.0f));
 
-        x = direction.x * sin;
-        y = direction.y * sin;
-        z = direction.z * sin;
+        x = direction.getX() * sin;
+        y = direction.getY() * sin;
+        z = direction.getZ() * sin;
         w = (float) Math.cos(Math.toRadians(angle / 2.0f));
     }
 
@@ -64,28 +64,5 @@ public class Quaternion {
         this.z = newZ;
         this.w = newW;
 
-    }
-
-    public float[] getRotationMatrix() {
-        float[] matrix = new float[16];
-
-        matrix[0]  = 1.0f - 2.0f * (y * y + z * z);
-        matrix[1]  = 2.0f * (x * y + z * w);
-        matrix[2]  = 2.0f * (x * z - y * w);
-        matrix[3]  = 0.0f;
-        matrix[4]  = 2.0f * (x * y - z * w);
-        matrix[5]  = 1.0f - 2.0f * (x * x + z * z);
-        matrix[6]  = 2.0f * (y * z + x * w);
-        matrix[7]  = 0.0f;
-        matrix[8]  = 2.0f * (x * z + y * w);
-        matrix[9]  = 2.0f * (y * z - x * w);
-        matrix[10] = 1.0f - 2.0f * (x * x + y * y);
-        matrix[11] = 0.0f;
-        matrix[12] = 0.0f;
-        matrix[13] = 0.0f;
-        matrix[14] = 0.0f;
-        matrix[15] = 1.0f;
-
-        return matrix;
     }
 }
